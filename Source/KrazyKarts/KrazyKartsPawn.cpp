4a99a07597817e3ee879a0083ef444c8589635a6
Gregory Scott Hanna <eagle_knight_22@hotmail.com>
2018-11-08 00:28:08 -0500

    Disabled VR plugins Restructured default directories and adjusted text references in cpp files Added new Pawn class (GoKart) and created a blueprint based on it. Created blueprint of the gamemode and set default pawn to be the new GoKart blueprint Added forward/backward movement to GoKart

--- Source/KrazyKarts/KrazyKartsPawn.cpp
+++ Source/KrazyKarts/KrazyKartsPawn.cpp
@@ -34,10 +34,10 @@ const FName AKrazyKartsPawn::LookRightBinding("LookRight");
 AKrazyKartsPawn::AKrazyKartsPawn()
 {
 	// Car mesh
-	static ConstructorHelpers::FObjectFinder<USkeletalMesh> CarMesh(TEXT("/Game/Vehicle/Sedan/Sedan_SkelMesh.Sedan_SkelMesh"));
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> CarMesh(TEXT("/Game/Static/Vehicle/Sedan/Sedan_SkelMesh"));
 	GetMesh()->SetSkeletalMesh(CarMesh.Object);
 
-	static ConstructorHelpers::FClassFinder<UObject> AnimBPClass(TEXT("/Game/Vehicle/Sedan/Sedan_AnimBP"));
+	static ConstructorHelpers::FClassFinder<UObject> AnimBPClass(TEXT("/Game/Static/Vehicle/Sedan/Sedan_AnimBP"));
 	GetMesh()->SetAnimInstanceClass(AnimBPClass.Class);
 	
 	// Simulation
